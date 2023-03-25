31e6012a7930e4b994e632b2081b766ab49235d9
Arseniy <Arsenchozzz@mail.ru>
2023-03-25 03:13:18 +0300

    Add project files.

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    public delegate double MathDelegate(double value1, double value2);
+
+    public static double Add(double value1, double value2)
+    {
+        return value1 + value2;
+    }
+    public static double Subtract(double value1, double value2)
+    {
+        return value1 - value2;
+    }
+
+    static void Main(string[] args)
+    {
+        // пример с математикой 
+        MathDelegate mathDelegate = Add;
+        var result = mathDelegate(5, 2);
+        Console.WriteLine(result);
+        // вывод: 7
+
+        mathDelegate = Subtract;
+        result = mathDelegate(5, 2);
+        Console.WriteLine(result);
+        // вывод: 3
+
+
+        // пример с библиотекой
+        StockExchangeMonitor.Monitor monitor = new();
+        monitor.PriceChangeHandler = ShowPrice;
+        monitor.Start();
+    }
+
+    // реализация (обращения) получения данных из класса 
+    private static void ShowPrice(int price)
+    {
+        Console.WriteLine($"New price is: {price}");
+    }
+}
---
+++ StockExchangeMonitor/Monitor.cs
@@ -0,0 +1,19 @@
+﻿namespace StockExchangeMonitor
+{
+    public class Monitor
+    {
+        public delegate void PriceChange(int price);
+        public PriceChange PriceChangeHandler { get; set; }
+
+        // имитация деятельности 
+        public void Start()
+        {
+            while (true)
+            {
+                int bankOfAmericaPrice = (new Random()).Next(100);
+                PriceChangeHandler(bankOfAmericaPrice);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
